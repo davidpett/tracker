f0c1154a43c40a2021f9eda02aab8710db8c3d10
David Pett <me@davidpett.com>
2014-11-12 16:22:43 -0600

    [added] pikaday for date-time support

--- app/models/match.js
+++ app/models/match.js
@@ -1,7 +1,13 @@
+/* globals moment */
+
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   home: DS.belongsTo('team', {async: true}),
   away: DS.belongsTo('team', {async: true}),
-  date: DS.attr('date')
+  date: DS.attr('date'),
+
+  formattedDate: function() {
+    return moment(this.get('date')).format('MMMM Do YYYY, h:mm a');
+  }.property('date')
 });
